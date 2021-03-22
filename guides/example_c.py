3de682207c0ca86d338fcec861f573973117e45f
emmanuel.charon <emmanuel.charon@enercoop.org>
2021-03-22 17:28:12 +0100

    modify guides

---
+++ guides/example_c.py
@@ -0,0 +1,8 @@
+# If you haven't already, read Example A and example B first.
+# In example_c.py, we show an example of actual python program that can be called
+# from the command line in order to train the algorithm or make predictions. 
+# However it does not contain data-retrieval functions : 
+# they are specific to each project & environment.
+
+
+# TODO
