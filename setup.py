3f6aedf602d7da04072a595b0f87b5c4902de3d0
clement.jeannesson <clement.jeannesson@enercoop.org>
2024-02-16 16:48:29 +0100

    [13](setup) Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,18 @@ setup(
     project_urls={"Bug Tracker": "https://github.com/enercoop/enda/issues"},
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-    python_requires=">=3.7.3",
-    install_requires=["pandas>=1.0.0"],
+    python_requires=">=3.7.3,<3.11",
+    install_requires=[
+        "pandas>=1.0.0,<2.0.0",
+        "h2o~=3.42.0.4",
+        "scikit-learn~=1.0.2",
+        "statsmodels<=0.13.5",
+        "bottleneck~=1.3.7",
+        "joblib~=1.3.0",
+        "jours-feries-france~=0.7.0",
+        "numexpr~=2.8.4",
+        "numpy~=1.18",
+        "unidecode~=1.3.6",
+        "vacances-scolaires-france~=0.10.0"
+    ]
 )
