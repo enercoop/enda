ea8c025c58ac1f15fe8808dabba76f9a632682f3
clement.jeannesson <clement.jeannesson@enercoop.org>
2024-03-26 09:55:19 +0100

    [17](notebooks) Adapt setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, "README.md"), "r") as f:
 # This call to setup() does all the work
 setup(
     name="enda",
-    version="0.0.8",
+    version="1.0.0",
     description="Tools to manipulate energy time-series and contracts, and to perform forecasts.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -27,17 +27,13 @@ setup(
     project_urls={"Bug Tracker": "https://github.com/enercoop/enda/issues"},
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-    python_requires=">=3.7.3,<3.11",
+    python_requires=">=3.9,<3.11",
     install_requires=[
-        "pandas>=1.0.0,<2.0.0",
-        "h2o~=3.42.0.4",
-        "scikit-learn~=1.0.2",
-        "statsmodels<=0.13.5",
-        "bottleneck~=1.3.7",
-        "joblib~=1.3.0",
+        "h2o>=3.36.2.2",
         "jours-feries-france~=0.7.0",
-        "numexpr~=2.8.4",
-        "numpy~=1.18",
+        "pandas>=1.4.0,<2.0.0",
+        "scikit-learn>=1.0.2",
+        "statsmodels<=0.13.5",
         "unidecode~=1.3.6",
         "vacances-scolaires-france~=0.10.0"
     ]
