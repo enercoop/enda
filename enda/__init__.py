12f8068d0f39a381a0f0735c662907a812171bf1
clement.jeannesson <clement.jeannesson@enercoop.org>
2024-03-29 16:32:52 +0100

    Added pdoc to the project + github actions workflow to github pages

--- enda/__init__.py
+++ enda/__init__.py
@@ -1,26 +1,8 @@
-""" init file for enda package"""
+"""
+.. include:: ../README.md
+"""
 
 __version__ = "1.0.0"
-
-# Let users know if they're missing any of our hard dependencies
-hard_dependencies = ("pandas","h2o","sklearn")
-# note that we also need python-dateutil and pytz but pandas already depends on them, so importing pandas is enough
-missing_dependencies = []
-
-for dependency in hard_dependencies:
-    try:
-        __import__(dependency)
-    except ImportError:
-        missing_dependencies.append(dependency)
-
-if len(missing_dependencies) > 0:
-    raise ImportError(
-        "Unable to import required dependencies:\n"
-        + "\n".join(
-            missing_dependencies,
-        )
-    )
-del hard_dependencies, dependency, missing_dependencies
 
 # import some subclasses here so users can use for instance :
 # 'enda.Contracts' without knowing the internal structure.
