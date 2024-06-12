d64d452d6bd7329efe51fc91b14ef2eed55cc6ff
clement.jeannesson <clement.jeannesson@enercoop.org>
2024-06-12 10:55:18 +0200

    [2287](publish) Expose classes directly

--- enda/__init__.py
+++ enda/__init__.py
@@ -2,17 +2,23 @@
 .. include:: ../README.md
 """
 
-__version__ = "1.0.0"
-
 # import some subclasses here so users can use for instance :
 # 'enda.Contracts' without knowing the internal structure.
-# Do not import classes that need a specific packages like "H2OModel".
 
 from enda.feature_engineering.calendar import Calendar, Holidays  # noqa
 from enda.feature_engineering.datetime_features import DatetimeFeature  # noqa
 
+from enda.ml_backends.h2o_estimator import EndaH2OEstimator
+from enda.ml_backends.sklearn_estimator import EndaSklearnEstimator
+
 from enda.backtesting import BackTesting  # noqa
 from enda.contracts import Contracts  # noqa
+from enda.estimators import (
+    EndaEstimator,
+    EndaNormalizedEstimator,
+    EndaEstimatorRecopy,
+    EndaEstimatorWithFallback,
+    EndaStackingEstimator)  # noqa
 from enda.power_predictor import PowerPredictor  # noqa
 from enda.power_stations import PowerStations  # noqa
 from enda.scoring import Scoring  # noqa
