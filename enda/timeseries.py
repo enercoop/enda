d6de435c416fb575bdd7b32586f4fb293429719a
clement.jeannesson <clement.jeannesson@enercoop.org>
2024-07-03 17:34:25 +0200

    [2545](post review) Fix post review

--- enda/timeseries.py
+++ enda/timeseries.py
@@ -5,7 +5,6 @@ This module is deprecated and has been moved in tools.timeseries
 import warnings
 from enda.tools.timeseries import TimeSeries
 
-# Optionally, you can add a deprecation warning
 warnings.warn("Module 'timeseries' is deprecated. Please use 'tools.timeseries' instead.",
               DeprecationWarning,
               stacklevel=2)
--- enda/timezone_utils.py
+++ enda/timezone_utils.py
@@ -5,7 +5,6 @@ This module is deprecated and has been moved in tools.timezone_utils
 import warnings
 from enda.tools.timezone_utils import TimezoneUtils
 
-# Optionally, you can add a deprecation warning
 warnings.warn("Module 'timezone_utils' is deprecated. Please use 'tools.timezone_utils' instead.",
               DeprecationWarning,
               stacklevel=2)
